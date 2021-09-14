2b8ab831fb57d4cb8bff74a5b8b0b0ff545fb92c
aimanfaris <xxfarisaiman@gmail.com>
2021-09-14 20:04:08 +0700

    coba variabel java

---
+++ pertemuan 3/CobaVar.java
@@ -0,0 +1,14 @@
+public class CobaVar {
+    public static void main(String [] args){
+        String namaDepan = "Faris";
+        String namaBelakang = "Aiman";
+        int umur = 18;
+        double tinggiBadan = 185;
+        double beratBadan = 68;
+
+        System.out.println("Nama : " + namaDepan + " " + namaBelakang);
+        System.out.println("Umur : " + umur + " tahun");
+        System.out.println("Tinggi Badan : " + (int)tinggiBadan + " cm");
+        System.out.println("Berat Badan : " + beratBadan + " kg");
+    }
+}
