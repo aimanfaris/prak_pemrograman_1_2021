a39fa84d1e9aa7dfac189d3f2201f9661294ea22
aimanfaris <xxfarisaiman@gmail.com>
2021-09-14 20:08:27 +0700

    coba variabel python

---
+++ pertemuan 3/cobavar.py
@@ -0,0 +1,10 @@
+namaDepan = "Faris"
+namaBelakang = "Aiman"
+umur = 18
+tinggiBadan = 1.85
+beratBadan = 69.5
+
+print("Nama saya adalah " + namaDepan + namaBelakang)
+print("Umur saya adalah " + str(umur) + " tahun")
+print("Tinggi badan saya adalah " + str(tinggiBadan) + " m")
+print("Tinggi badan saya adalah " + str(beratBadan) + " kg")
