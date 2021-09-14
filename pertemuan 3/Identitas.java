debdf1dad42a8344bdd9ad600673a1e3e80c6949
aimanfaris <xxfarisaiman@gmail.com>
2021-09-14 20:54:20 +0700

    tugas identitas

---
+++ pertemuan 3/Identitas.java
@@ -0,0 +1,19 @@
+public class Identitas {
+    public static void main(String [] args){
+        String nama = "Faris Aiman";
+        String nim = "21537144033";
+        String programStudi = "Teknologi Informasi";
+        String jurusan = "PTEI";
+        String fakultas = "Teknik";
+        double ipk = 3.55;
+        int angkatan = 2021;
+        
+        System.out.println("Nama saya adalah : " + nama);
+        System.out.println("nim saya : " + nim );
+        System.out.println("saya dari program studi :" + programStudi);
+        System.out.println("jurusan :" + jurusan);
+        System.out.println("Fakultas :" + fakultas);
+        System.out.println("Saya ingin lulus dengan ipk :" + ipk);
+        System.out.println("Saya angkatan :" + angkatan);
+    }
+}
---
+++ pertemuan 3/identitas.py
@@ -0,0 +1,15 @@
+nama = "Faris Aiman"
+nim = "21537144033"
+programStudi = "Teknologi Informasi"
+jurusan = "PTEI"
+fakultas = "Teknik"
+ipk = "3.55"
+angkatan = "2021"
+
+print("Nama saya adalah :" + nama)
+print("Nim saya :" + nim)
+print("Saya dari program studi :" + programStudi)
+print("Jurusan :" + jurusan)
+print("Fakultas :")
+print("Saya ingin lulus dengan ipk :" + ipk)
+print("saya angkatan" + angkatan)
