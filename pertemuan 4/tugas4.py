f1aa6276800d13f5b648c3cd5156ec2c232d48a6
aimanfaris <xxfarisaiman@gmail.com>
2021-09-21 21:28:31 +0700

    tugas 4 java py

---
+++ pertemuan 4/tugas4.py
@@ -0,0 +1,28 @@
+nilaiA = int(input('Masukan nilai a : '))
+nilaiB = int(input('Masukan nilai b : '))
+
+# Pertambahan
+print("Hasil ", end=" ")
+print('{} + {} = '.format(nilaiA, nilaiB), end=" ")
+print(nilaiA + nilaiB)
+
+# Pengurangan
+print("Hasil ", end=" ")
+print('{} - {} = '.format(nilaiA, nilaiB), end=" ")
+print(nilaiA - nilaiB)
+
+# Perkalian
+print("Hasil ", end=" ")
+print('{} * {} = '.format(nilaiA, nilaiB), end=" ")
+print(nilaiA * nilaiB)
+
+# Pembagian
+print("Hasil ", end=" ")
+print('{} / {} = '.format(nilaiA, nilaiB), end=" ")
+print(nilaiA / nilaiB)
+
+# Modulusprint
+print("Hasil ", end=" ")
+print('{} % {} = '.format(nilaiA, nilaiB), end=" ")
+print(nilaiA % nilaiB), 
+
