4b38df3b8825fd8ff9e868e61e7751edcef3f253
aimanfaris <xxfarisaiman@gmail.com>
2021-09-21 21:27:48 +0700

    coba operator java py

---
+++ pertemuan 4/Cobaoperator.java
@@ -0,0 +1,19 @@
+public class Cobaoperator {
+    public static void main(String[] args){
+        int x = 10;
+        double y = 6;
+        double hasil = x / y;
+        System.out.println("Hasil = " + hasil);
+        x = x + 1;
+        System.out.println("Hasil x = " + x);
+        ++x;
+        System.out.println("Hasil x = " + x);
+        x++;
+        System.out.println("Hasil x = " + x);
+        System.out.println("Hasil x = " + ++x);
+        x = x + 5;
+        System.out.println("Hasil x = " + x);
+        x += 5;
+        System.out.println("Hasil x = " + x);
+    }
+}
