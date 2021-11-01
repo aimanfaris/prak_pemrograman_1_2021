2709477423dd42c020b7c38b6a111f76fc43cd08
aimanfaris <xxfarisaiman@gmail.com>
2021-11-01 13:30:40 +0700

    belajar jumlah tanpa array

--- pertemuan 8/TanpaArray.java
+++ pertemuan 8/TanpaArray.java
@@ -4,14 +4,18 @@ public class TanpaArray {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Masukkan bilangan ke-1 : ");
-        int bilangan1 = scanner.nextInt();
-        System.out.print("Masukkan bilangan ke-2 : ");
-        int bilangan2 = scanner.nextInt();
-        System.out.print("Masukkan bilangan ke-3 : ");
-        int bilangan3 = scanner.nextInt();
+        System.out.print("Masukkan angka pertama = ");
+        int angka1 = scanner.nextInt();
+        System.out.print("Masukkan angka kedua = ");
+        int angka2 = scanner.nextInt();
+        System.out.print("Masukkan angka ketiga = ");
+        int angka3 = scanner.nextInt();
+        System.out.print("Masukkan angka keempat = ");
+        int angka4 = scanner.nextInt();
+        System.out.print("Masukkan angka kelima = ");
+        int angka5 = scanner.nextInt();
 
-        int jumlah = bilangan1 + bilangan2 + bilangan3;
-        System.out.println("Jumlah = " + jumlah);
+        int jumlah = angka1 + angka2 + angka3 + angka4 + angka5;
+        System.out.println("Jumlah angka yang anda masukan adalah = " + jumlah);
     }
 }
